48c095c56376cdc67570f85e026ad1f4427becf4
Harry <hjwp2@cantab.net>
2016-12-01 20:25:43 +0000

    new view for new_list. --ch06l023--

--- lists/views.py
+++ lists/views.py
@@ -8,6 +8,11 @@ def home_page(request):
     return render(request, 'home.html')
 
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
+
+
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
