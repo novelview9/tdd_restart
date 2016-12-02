hjwp/book-example

use error constant in forms test. --ch11l012--

lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.forms import ItemForm
+from lists.forms import EMPTY_LIST_ERROR, ItemForm
 
 
 class ItemFormTest(TestCase):
@@ -14,7 +14,5 @@ class ItemFormTest(TestCase):
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['text'],
-            ["You can't have an empty list item"]
-        )
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+

novelview9/tdd_restart

lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.forms import ItemForm
+from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 
 
 class ItemFormTest(TestCase):
@@ -14,7 +14,5 @@ class ItemFormTest(TestCase):
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['text'],
-            ["You can't have an empty list item"]
-        )
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+