hjwp/book-example

use our new dupe item error constant in tests. --ch12l049--

lists/tests/test_views.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from django.utils.html import escape
-from unittest import skip
 
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR,
+    ExistingListItemForm, ItemForm,
+)
 from lists.models import Item, List
 
 
@@ -150,7 +152,6 @@ class ListViewTest(TestCase):
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
 
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -159,7 +160,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)

novelview9/tdd_restart

lists/tests/test_views.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from django.utils.html import escape
-from unittest import skip
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
+)
 from lists.models import Item, List
 
 
@@ -144,7 +146,6 @@ class ListViewTest(TestCase):
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -153,7 +154,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)