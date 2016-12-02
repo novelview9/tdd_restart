hjwp/book-example

Create base FT class and a class for each test. --ch10l002--

functional_tests/tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import sys
 from unittest import skip
 
-class NewVisitorTest(LiveServerTestCase):
+class FunctionalTest(LiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -33,6 +33,9 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+
+
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -107,6 +110,9 @@ class NewVisitorTest(LiveServerTestCase):
         # Satisified, they both go back to sleep
 
 
+
+class LayoutAndStylingTest(FunctionalTest):
+
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.server_url)
@@ -130,6 +136,9 @@ class NewVisitorTest(LiveServerTestCase):
             delta=5
         )
 
+
+
+class ItemValidationTest(FunctionalTest):
 
     @skip
     def test_cannot_add_empty_list_items(self):

novelview9/tdd_restart

functional_tests/tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import sys
 from unittest import skip
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class FunctionalTest(StaticLiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -20,6 +20,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         if cls.server_url == cls.live_server_url:
             super().tearDownClass()
 
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -33,6 +34,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+
+
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -120,6 +124,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Satisfied, they both go back to sleep
 
 
+
+class LayoutAndStylingTest(FunctionalTest):
+
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.server_url)
@@ -143,6 +150,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=5
         )
 
+
+
+class ItemValidationTest(FunctionalTest):
 
     @skip
     def test_cannot_add_empty_list_items(self):